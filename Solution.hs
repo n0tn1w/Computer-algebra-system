21542283fad4a2e0fc8bdd8d88dcd8a3b393bb2f
n0tn1w <somedpm@gmail.com>
2024-02-15 11:07:49 +0200

    Final project

--- Solution.hs
+++ Solution.hs
@@ -1,21 +1,8 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
--- cover all cases!
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
--- warn about incomplete patterns v2
-{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
--- write all your toplevel signatures!
-{-# OPTIONS_GHC -fwarn-missing-signatures #-}
--- use different names!
-{-# OPTIONS_GHC -fwarn-name-shadowing #-}
--- use all your pattern matches!
-{-# OPTIONS_GHC -fwarn-unused-matches #-}
-
 module Solution where
 import Data.Char
 import Text.Parsec
 import Text.Parsec.String (Parser)
 import qualified Text.Parsec as Parsec (many)
-
 
 data Expr
   = Var String
@@ -38,8 +25,16 @@ data FunType
   deriving (Show)
 
 eval :: Expr -> Maybe Double
+eval (Var _) = Nothing
 eval (Val n) = evalNumber n
 eval (Oper op e1 e2) = applyOperation op (eval e1) (eval e2)
+eval (Function fT e1) = applyFunction fT (eval e1)
+
+applyFunction :: FunType -> Maybe Double -> Maybe Double
+applyFunction fT (Just x) =
+  case fT of
+    Derivative -> Just x
+    Integration -> Just 0 
 
 evalNumber :: Number -> Maybe Double
 evalNumber (IntegerNum x) = Just (fromIntegral x)
@@ -53,6 +48,28 @@ applyOperation op (Just x) (Just y) =
     Subtract -> Just (x - y)
     Divide -> if y /= 0 then Just (x / y) else Nothing
     Power -> Just (x ** y)
+applyOperation op _ _ = Nothing
+
+evalOperation :: OperType -> Either Double Expr -> Either Double Expr -> Expr
+evalOperation Add (Left x) (Left y) = Val (DecimalNum (x + y))
+evalOperation Multiply (Left x) (Left y) = Val (DecimalNum (x * y))
+evalOperation Subtract (Left x) (Left y) = Val (DecimalNum (x - y))
+evalOperation Divide (Left x) (Left y)
+  | y /= 0 = Val (DecimalNum (x / y))
+  | otherwise = Val (DecimalNum 0) 
+evalOperation Power (Left x) (Left y) = Val (DecimalNum (x ** y))
+
+partialEval :: Expr -> Expr
+partialEval (Var x) = Var x
+partialEval (Val n) = Val n
+partialEval (Oper op e1 e2) =
+  case (eval e1, eval e2) of
+    (Just x, Just y) ->
+      case applyOperation op (Just x) (Just y) of
+        Just result -> Val (DecimalNum result)
+    _ -> Oper op (partialEval e1) (partialEval e2)
+partialEval (Function f e) = Function f (partialEval e)
+
 
 numberParser :: Parser Number
 numberParser = try decimalNumber <|> integerNumber
@@ -91,3 +108,19 @@ exprParser = spaces *> termParser <* spaces
 parens :: Parser a -> Parser a
 parens = between (char '(' *> spaces) (char ')' *> spaces)
 
+calculate :: Expr -> IO ()
+calculate expr = do
+  let result = eval expr
+  case result of
+    Just value -> print value
+    Nothing -> print (partialEval expr)
+
+main :: IO ()
+main = do
+  putStrLn "Enter an expression:"
+  input <- getLine
+  case parse exprParser "" input of
+    Left err -> print err
+    Right expr -> do
+      putStrLn "Result:"
+      calculate expr
